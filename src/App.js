12ca853fd49cd4ccccf90e1e00218f2ef3dbd252
rokf1997 <69386569+rokf1997@users.noreply.github.com>
2020-08-08 13:06:54 +0200

    Update App.js
    test commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -22,5 +22,5 @@ function App() {
     </div>
   );
 }
-
+//nik je najboljsi programer na svetu
 export default App;
